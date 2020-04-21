b15e5aa7721500684c775f2b308d9c9d7e87a7a0
02zoetho <62854195+02zoetho@users.noreply.github.com>
2020-04-21 15:03:54 +0200

    Addier Methode

--- Minirechner/Minirechner/Program.cs
+++ Minirechner/Minirechner/Program.cs
@@ -9,7 +9,20 @@ namespace Minirechner
     {
         static void Main(string[] args)
         {
-           
+            int a = 100;
+            int b = 10;
+
+            Addiere(a, b);
+          
+
+        }
+
+        static int Addiere(int Zahl1, int Zahl2)
+        {// Addier Methode
+
+            Zahl1 = Zahl1 + Zahl2;
+            return Zahl1;
+
         }
     }
 }
