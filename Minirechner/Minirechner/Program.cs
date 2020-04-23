8d3511145defb81dfbf859cd1e37093a0a8ece16
02zoetho <62854195+02zoetho@users.noreply.github.com>
2020-04-23 11:54:37 +0200

    Multiplications Methode

--- Minirechner/Minirechner/Program.cs
+++ Minirechner/Minirechner/Program.cs
@@ -9,34 +9,20 @@ namespace Minirechner
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             int a = 100;
             int b = 10;
 
-            Addiere(a, b);
+            Multipliziere(a, b);
           
 
         }
 
-        static int Addiere(int Zahl1, int Zahl2)
+        static int Multipliziere(int Zahl1, int Zahl2)
         {// Addier Methode
 
-            Zahl1 = Zahl1 + Zahl2;
+            Zahl1 = Zahl1 * Zahl2;
             return Zahl1;
-
-=======
-            // Subbtractions Methode
-            int a = 100;
-            int b = 10;
-
-            Subtrahiere(a,b);
-        }
-
-        static int Subtrahiere(int Zahl1, int Zahl2)
-        {
-            Zahl1 = Zahl1 - Zahl2;
-            return Zahl1;
->>>>>>> Subb
+            
         }
     }
 }
