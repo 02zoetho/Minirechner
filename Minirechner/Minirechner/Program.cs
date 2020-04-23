55b759c5121a191f07cddb48e576d0985c3f32fb
02zoetho <62854195+02zoetho@users.noreply.github.com>
2020-04-23 11:58:43 +0200

    Divisions Methode

--- Minirechner/Minirechner/Program.cs
+++ Minirechner/Minirechner/Program.cs
@@ -12,15 +12,15 @@ namespace Minirechner
             int a = 100;
             int b = 10;
 
-            Multipliziere(a, b);
+            Dividiere(a, b);
           
 
         }
 
-        static int Multipliziere(int Zahl1, int Zahl2)
+        static int Dividiere(int Zahl1, int Zahl2)
         {// Addier Methode
 
-            Zahl1 = Zahl1 * Zahl2;
+            Zahl1 = Zahl1 / Zahl2;
             return Zahl1;
             
         }
